044a8a6c2f9d2cdb6de040d7276a270f5016ffd6
Charles Mlengela <cmlengela@my.bcit.ca>
2019-11-21 18:44:00 -0800

    skeleto and class scripts

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("menu");
+var body = document.body;
+console.count(button);
+
+button.addEventListener('click', function(){    
+    body.classList.toggle('open');
+ });
+ var items = ['Home','About The Course','Schedule','Schedule','Contact']
+
