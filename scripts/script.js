f07188958d19fe384d7802587953cefc620c451d
Charles Mlengela <cmlengela@my.bcit.ca>
2019-11-25 16:47:55 -0800

    lesson page and lesson3 collapse images

--- scripts/script.js
+++ scripts/script.js
@@ -19,10 +19,12 @@ var view = document.getElementsByClassName("lesson-section");
 var arr = [...view];
 
 arr.forEach(element => {
-
 	element.addEventListener("click", ()=> {
 		var info = element.children[1];
+		var header = element.children[0];
+		var set = 0;
 		info.classList.toggle("view-info");
+		header.children[1].classList.toggle("image");
 	}); 
 });
 
