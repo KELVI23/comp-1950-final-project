95b9d943aa9ad3d744de4392a139472e562d633a
Kelvin Musodza <kelvinledoit@gamil.com>
2019-12-04 00:35:00 -0800

    desktop nav and contact form

--- thank-you.php
+++ thank-you.php
@@ -6,7 +6,40 @@ $pgClass = 'pg-thank-you';
 include './templates/header.php';
 
 ?>
+
+
 <body class="wrapper">
+    <div class="page-wrapper">
+        <nav >
+            <div class="nav">
+                <div class="banner-nav">
+                    <div class="nav-toggle">
+                        <div class="nav-toggle-bar">
+                        </div>
+                    </div>
+                </div>
+                
+                <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About the Course</a></li>
+                        <li><a href="schedule.html">Schedule</a></li>
+                        <li><a href="lessons.html">Lessons</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                </ul>   
+            </div>
+            <div class="bcit-mobile-navigation">
+                    <img src="images/bcit-logo.svg" alt="">
+            </div>
+            <section class="desktop-nav">
+                <ul >
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About the Course</a></li>
+                  <li><a href="schedule.html">Schedule</a></li>
+                  <li><a href="lessons.html">Lessons</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+              </ul>   
+              </section>
+        </nav>  
     <section class="thank-you">
 
         <h1>Thank You</h1>
@@ -20,9 +53,32 @@ include './templates/header.php';
         <?php endif ?>
 
     </section>
+    <footer>
+        <h2 class="course-header">Course Info</h2>
+        <ul class="course">
+            <li><a href="#">Outline</a></li>
+            <li><a href="#">Schedule</a></li>
+            <li><a href="#">Lessons</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <h2 class="reg-header">Registration Info</h2>
+        <ul class="registration">
+            <li><a href="#0">Talk With An Advisor</a></li>
+            <li><a href="#0">Sign Up For a Tour</a></li>
+            <li><a href="#0">Attend An Info Night</a></li>
+            <li><a href="#0">Register</a></li>
+        </ul>
+        <h2 class="address">Find Us</h2>
+        <div class="address-info">
+            <p>British Columbia Institue of Technology</p>
+            <p>3700 Willingdon Avenue</p>
+            <p>Burnaby, British Columbia</p>
+            <p>Canada, V5G 3H2</p>
+        </div>
+      </footer> 
         </body>  
 <?php 
 
-include './templates/footer.php';
+// include './templates/footer.php';
 
 ?>
